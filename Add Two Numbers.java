2f917b937b152bbdc77ae302e163b74c98dd7289
SHIVHARI LOKHANDE <33317928+shivharilokhande@users.noreply.github.com>
2023-01-25 21:11:22 +0530

    Update Add Two Numbers.java

--- Add Two Numbers.java
+++ Add Two Numbers.java
@@ -28,11 +28,12 @@ Constraints:
 The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
-*/ 
-  
 
- Definition for singly-linked list.
- public class ListNode {
+  
+Definition for singly-linked list.
+*/
+
+public class ListNode {
       int val;
       ListNode next;
       ListNode() {}
